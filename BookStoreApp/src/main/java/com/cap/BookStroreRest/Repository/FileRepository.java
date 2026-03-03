4dc6544f797515382dbff52e9c667cd5fd6b6a06
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-03-03 16:01:27 +0530

    Extended FileRepository with JpaRepository

--- BookStoreApp/src/main/java/com/cap/BookStroreRest/Repository/FileRepository.java
+++ BookStoreApp/src/main/java/com/cap/BookStroreRest/Repository/FileRepository.java
@@ -1,4 +1,7 @@
 package com.cap.BookStroreRest.Repository;
 
-public interface FileRepository {
+import com.cap.BookStroreRest.Entity.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepository extends JpaRepository<FileEntity, Long> {
 }
