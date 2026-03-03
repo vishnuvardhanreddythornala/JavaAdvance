11d50050f47ee74072ce40974a6e00128a28d223
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-03-03 15:59:06 +0530

    Implemented  pagination and sorting in BookService

--- BookStoreApp/src/main/java/com/cap/BookStroreRest/Service/BookService.java
+++ BookStoreApp/src/main/java/com/cap/BookStroreRest/Service/BookService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.data.domain.Pageable;
 import java.util.List;
-import java.util.Optional;
+
 
 @RequiredArgsConstructor
 @Service
