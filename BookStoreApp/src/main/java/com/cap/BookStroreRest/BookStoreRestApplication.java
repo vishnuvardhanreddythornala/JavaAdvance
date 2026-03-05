e27ee001b07a2c3da2a8611e2b7289d69bc61426
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-03-05 10:22:56 +0530

    Enabled Caching

--- BookStoreApp/src/main/java/com/cap/BookStroreRest/BookStoreRestApplication.java
+++ BookStoreApp/src/main/java/com/cap/BookStroreRest/BookStoreRestApplication.java
@@ -2,9 +2,11 @@ package com.cap.BookStroreRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 //@AutoConfiguraion, @ComponentScan, @Configuration, @Documented
+@EnableCaching
 public class BookStoreRestApplication {
 
 	public static void main(String[] args) {
