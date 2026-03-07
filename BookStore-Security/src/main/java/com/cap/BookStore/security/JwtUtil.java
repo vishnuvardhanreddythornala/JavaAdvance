456347943fe9fac942f5c876f42352b4532a9699
vishnuvardhanreddythornala <vishnuvardhanreddythornala@gmail.com>
2026-03-07 11:55:19 +0530

    Implemented JwtUtil to generate token

--- BookStore-Security/src/main/java/com/cap/BookStore/security/JwtUtil.java
+++ BookStore-Security/src/main/java/com/cap/BookStore/security/JwtUtil.java
@@ -1,7 +1,53 @@
 package com.cap.BookStore.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+
 public class JwtUtil {
-    public String generateToken(String username, String name) {
-        return username;
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
+
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .subject(username)
+                .claim("role",role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+    private Claims getClaims(String token){
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public String extractUsername(String token){
+        return getClaims(token).getSubject();
+    }
+    public boolean validateToken(String token){
+        try{
+            getClaims(token);
+            return true;
+        }catch (JwtException e){
+            return  false;
+        }
+    }
+
 }
